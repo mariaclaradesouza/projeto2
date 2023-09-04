db541952efd84a11f13f0757e7df3b94aad23187
mariaclaradesouza <mariacfernandesval@gmail.com>
2023-09-04 16:52:55 -0400

    oie

---
+++ src/topo.js
@@ -0,0 +1,25 @@
+import logo from './aacu.png';
+import './App.css';
+
+function Topo() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          yuuumeeeeeee
+        </p>
+        <a
+          className="App-link"
+          href="https://www.leagueoflegends.com/pt-br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          lol
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Topo;
